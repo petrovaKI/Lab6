b9213a063410435c38f4c8ded25180eb2fe87e4c
Kseniya <ksyushki5@yandex.ru>
2022-04-16 12:26:54 -0500

    commit 12

--- sources/Logging.cpp
+++ sources/Logging.cpp
@@ -3,30 +3,30 @@
 #include "Logging.hpp"
 
 void set_logs(){
-  // Событие с уровнем логирования - trace
-  auto TraceFileLogger = boost::log::add_file_log(
+  //уровень логирования - trace
+  auto trace_log = boost::log::add_file_log(
       boost::log::keywords::file_name =
           "/home/kseniya/lab6/logs/"
           "TraceLog_%N.log",
       boost::log::keywords::rotation_size = 10 * 1024 * 1024,
       boost::log::keywords::format =
           "[%TimeStamp%][%Severity%][%ThreadID%]: %Message%");
-  // Событие с уровнем логирования - info
-  auto InfoFileLogger = boost::log::add_file_log(
+  //уровень логирования - info
+  auto info_log = boost::log::add_file_log(
       boost::log::keywords::file_name =
           "/home/kseniya/lab6/logs/"
           "TraceLog_%N.log",
       boost::log::keywords::format =
           "[%TimeStamp%][%Severity%][%ThreadID%]: %Message%");
-  // Событие вывода в консоль
-  auto consoleLogger = boost::log::add_console_log(
+  //вывод в консоль
+  auto console_log = boost::log::add_console_log(
       std::cout, boost::log::keywords::format =
                      "[%TimeStamp%][%Severity%][%ThreadID%]: %Message%");
-
-  TraceFileLogger->set_filter(boost::log::trivial::severity ==
+  //выставляем фильтры на логи
+  trace_log->set_filter(boost::log::trivial::severity ==
                               boost::log::trivial::trace);
-  InfoFileLogger->set_filter(boost::log::trivial::severity ==
+  info_log->set_filter(boost::log::trivial::severity ==
                              boost::log::trivial::info);
-  consoleLogger->set_filter(boost::log::trivial::severity ==
+  console_log->set_filter(boost::log::trivial::severity ==
                             boost::log::trivial::info);
 }
